304b190cf61a73051370d13d81d856cc6513d3f1
HynDuf7 <chauhuynhnoc@gmail.com>
2022-07-30 17:22:36 +0700

    Migrate cmd options to DictionaryManagement + Misc cleans
    - Merge UserInput to Helper. Helper now contains a global Scanner for an
      only one Scanner to read input from cmd (many scanner of the same
      stream is a bad practice)

--- src/main/java/dictionary/App.java
+++ src/main/java/dictionary/App.java
@@ -1,106 +1,22 @@
 package dictionary;
 
-import dictionary.server.Dictionary;
-import dictionary.server.UserInput;
+import dictionary.server.DictionaryManagement;
+import dictionary.server.Helper;
+
 public class App {
-    public static Dictionary dictionary;
-    public static UserInput userInput;
 
     private static final int LOOK_UP = 1;
     private static final int SHOW_ALL = 2;
-    private static final int INSERT = 3;
+    private static final int INSERT_CMD = 3;
     private static final int DELETE = 4;
     private static final int UPDATE = 5;
-
-    private static final int ADVANCED = 6;
+    private static final int INSERT_FILE = 6;
     private static final int EXIT = 7;
-
-    /** Option 1. Look up a word's definition. */
-    public static void lookUpWord() {
-        System.out.print("==> Enter the English word to look up: ");
-        String target = userInput.readLine();
-        String definition = dictionary.lookUpWord(target);
-        if (definition.equals("404")) {
-            System.out.println("The word you looked up isn't in the dictionary!\n");
-        } else {
-            System.out.println("Definition of `" + target + "`: " + definition + '\n');
-        }
-    }
-
-    /** Option 2. Show all words currently in the dictionary. */
-    public static void showAllWords() {
-        System.out.println(dictionary.getAllWords());
-    }
-
-    /** Add new words from the command line. */
-    public static void insertFromCommandline() {
-        System.out.print("==> Number of words to insert: ");
-        int numWords = userInput.readInteger();
-        userInput.readLine();
-        for (int i = 0; i < numWords; ++i) {
-            System.out.print("==> Word target: ");
-            String target = userInput.readLine();
-            System.out.print("==> Word definition: ");
-            String definition = userInput.readLine();
-            if (!dictionary.insertWord(target, definition)) {
-                System.out.println("`" + target + "` already exists in the dictionary!");
-            }
-        }
-    }
-    /** Call insertFromFile(),showAllWords(), dictionaryLookup(). */
-    public static void dictionaryAdvanced() throws  Exception {
-        System.out.println("Choose an option to proceed:");
-        System.out.println("1. Look up");
-        System.out.println("2. Show all words");
-        System.out.println("3. Insert from file");
-        int selection = userInput.readInteger();
-        userInput.readLine();
-        switch (selection) {
-            case LOOK_UP:
-                lookUpWord();
-                break;
-            case SHOW_ALL:
-                showAllWords();
-                break;
-            case INSERT:
-                DictionaryManagement.insertFromFile();
-        }
-    }
-
-    /** Delete a word. */
-    public static void deleteWord() {
-        System.out.print("==> Enter the English word to delete: ");
-        String target = userInput.readLine();
-        if (dictionary.deleteWord(target)) {
-            System.out.println("`" + target + "` successfully deleted.\n");
-        } else {
-            System.out.println("The word you delete isn't in the dictionary!\n");
-        }
-    }
-
-    /** Update a word's definition. */
-    public static void updateWord() {
-        System.out.print("==> Enter the English word to update: ");
-        String target = userInput.readLine();
-        System.out.print("==> Enter the new definition for `" + target + "`: ");
-        String definition = userInput.readLine();
-        if (dictionary.updateWordDefinition(target, definition)) {
-            System.out.println("`" + target + "` successfully updated.\n");
-        } else {
-            System.out.println("The word you update isn't in the dictionary!\n");
-        }
-    }
-
-    /** Exit the application. */
-    public static void exitApplication() {
-        System.out.println("Exiting...");
-        System.exit(0);
-    }
 
     /**
      * Display options for the commandline application.
      *
-     * @return the selected options (from 1 to 6)
+     * @return the selected options (from 1 to 7)
      */
     public static int displayOptions() {
         while (true) {
@@ -110,13 +26,15 @@ public class App {
             System.out.println("3. Insert words");
             System.out.println("4. Delete a word");
             System.out.println("5. Update a word's definition");
-            System.out.println("6. Advanced");
+            System.out.println("6. Insert from file");
             System.out.println("7. Exit");
             System.out.print("==> Enter a selection (1-7): ");
-            int selection = userInput.readInteger();
-            userInput.readLine();
-            if (1 <= selection && selection <= 6) {
+            int selection = Helper.readInteger();
+            Helper.readLine();
+            if (1 <= selection && selection <= 7) {
                 return selection;
+            } else {
+                System.out.println("Invalid option!");
             }
         }
     }
@@ -126,28 +44,28 @@ public class App {
      *
      * @param selection the chosen selection
      */
-    public static void executeSelection(int selection) throws Exception {
+    public static void executeSelection(int selection) {
         switch (selection) {
             case LOOK_UP:
-                lookUpWord();
+                DictionaryManagement.lookUpWord();
                 break;
             case SHOW_ALL:
-                showAllWords();
+                DictionaryManagement.showAllWords();
                 break;
-            case INSERT:
-                insertFromCommandline();
+            case INSERT_CMD:
+                DictionaryManagement.insertFromCommandline();
                 break;
             case DELETE:
-                deleteWord();
+                DictionaryManagement.deleteWord();
                 break;
             case UPDATE:
-                updateWord();
+                DictionaryManagement.updateWord();
                 break;
-            case ADVANCED:
-                dictionaryAdvanced();
+            case INSERT_FILE:
+                DictionaryManagement.insertFromFile();
                 break;
             case EXIT:
-                exitApplication();
+                DictionaryManagement.exitApplication();
                 break;
             default:
                 System.out.println("Invalid option!");
@@ -160,9 +78,7 @@ public class App {
      *
      * @param args cmd arguments
      */
-    public static void main(String[] args) throws Exception {
-        dictionary = new Dictionary();
-        userInput = new UserInput(System.in);
+    public static void main(String[] args) {
         while (true) {
             int selection = displayOptions();
             executeSelection(selection);
